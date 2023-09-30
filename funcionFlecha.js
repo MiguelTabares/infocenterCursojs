7b1f1dde0eace8ad8e41c57abfcf8c5149e2d54b
MiguelTabares <68822677+MiguelTabares@users.noreply.github.com>
2023-09-30 09:17:56 -0500

    Update funcionFlecha.js

--- funcionFlecha.js
+++ funcionFlecha.js
@@ -1,1 +1,11 @@
+const ages = [32, 33, 16, 40, 80, 13, 22, 17, 15, 8];
+//FUNCIÓN FLECHA del método Filter()
+const result = ages.filter((ages) => ages >= 18);
+console.log(result);
+
+/*
+function checkAdult(age) {
+  return age >= 18;
+}
+*/
 
