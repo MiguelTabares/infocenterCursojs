1fd719d0131684eeaf25ab09bfc37f05cf438b50
MiguelTabares <68822677+MiguelTabares@users.noreply.github.com>
2023-09-30 10:09:24 -0500

    programacion funcional con JS, uso de reduce y map

--- arrayFunction.js
+++ arrayFunction.js
@@ -1,1 +1,22 @@
+// utilizamos reduce para sumar el total de estrellas
+const galaxies = [ 
+{ name: 'Galaxy A', stars: 100 },
+{ name: 'Galaxy B', stars: 200 },
+{ name: 'Galaxy C', stars: 300 } ]
 
+const countStars = (galaxies) => { 
+  const total = galaxies.reduce((a,g) => a + g.stars, 0 )
+  return total
+}
+var resultado = countStars(galaxies)
+console.log(resultado)
+
+// map no transforma el array, sino que lo recorre para hacer algo sobre el
+// en este caso extraemos el nombre del objeto a través de el key name del objeto
+function galaxyNames(galaxies){
+  return galaxies.map(
+    (objeto) => objeto.name
+  )
+}
+var resultado2 = galaxyNames(galaxies)
+console.log(resultado2)
