03b55c3a736b9afab90fc03affa58230e2af980f
MiguelTabares <68822677+MiguelTabares@users.noreply.github.com>
2023-10-02 20:52:46 -0500

    Update octubre2.js

--- octubre2.js
+++ octubre2.js
@@ -1,1 +1,52 @@
+function getNestedRecordsExample(){
+  return [
+    {
+      id: 1,
+      name: 'Record 1',
+      details: {
+        description: 'Details for Record 1',
+        nestedInfo: {
+          value: 'Value for Record 1',
+        },
+      },
+    },
+    {
+      id: 2,
+      name: 'Record 2',
+      details: {
+        description: 'Details for Record 2',
+        nestedInfo: {
+          value: 'Value for Record 2',
+        },
+      },
+    },
+    {
+      id: 3,
+      name: 'Record 3',
+      details: {
+        description: 'Details for Record 3',
+        nestedInfo: {
+          value: 'Value for Record 3',
+        },
+      },
+    },
+  ];
+}
 
+function getNestedInfo(recordId){
+  const records = getNestedRecordsExample()
+  const record  = records.find(
+    (r) => r.id === recordId )
+  if(record){
+    return record.details.nestedInfo;
+  } else {
+    return null  
+  }
+}
+
+const id = 2;
+const nestedInfo = getNestedInfo(id);
+console.log(nestedInfo)
+
+//var a = getNestedRecordsExample();
+//console.log(a)
