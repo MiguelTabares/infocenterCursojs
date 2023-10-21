5ad42d267520abac7ef1e40e4697cbf9e0492501
Miguel Tabares <silenceisbeautyofsoul@gmail.com>
2023-10-21 17:09:16 -0500

    evento on click en js

---
+++ septiembre22.js
@@ -0,0 +1,9 @@
+// EVENTOS EN JS
+
+// Los eventos son acciones que puede ejecutar el usuario.
+
+// Un prompt es un input o también un mensaje que consiste en un pop-up o recuadro emergente
+function promptMe(){
+    var entradaUsuario = prompt("Ingrese por favor su nombre: ");
+    alert (entradaUsuario);
+}
